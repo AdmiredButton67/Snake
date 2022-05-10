5fe53f2ad13b3dc7b1f400891f32bdc4a7093da1
AdmiredButton67 <94034532+AdmiredButton67@users.noreply.github.com>
2022-05-10 10:03:00 -0500

    Añadí nombre

--- Snake.py
+++ Snake.py
@@ -2,6 +2,8 @@
 
 Exercises
 
+Nombre Luis Armando Mandujano Chávez
+
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
 3. How would you move the food?
